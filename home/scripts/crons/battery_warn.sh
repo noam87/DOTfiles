957fd35c19e46bd5dca247f73a599118f08c2011
noam <clusterfoo@server.fake>
2013-07-27 16:00:26 -0400

    More power management stuff

--- home/scripts/crons/battery_warn.sh
+++ home/scripts/crons/battery_warn.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-threshold=92
+threshold=4
 level=$(acpi | cut -f2 -d, | cut -f1 -d%)
 
 if [ $level -lt $threshold ]; then
-  DISPLAY=:0 DISPLAY=:0 zenity --info --text 'Battery low. Sleep in 1 minute.'&
+  DISPLAY=:0 DISPLAY=:0 zenity --info --text 'Battery low.'&
   # or use notify-send
-  sleep 2
-  pm-suspend
 else
   exit 0
 fi
