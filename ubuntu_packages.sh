c0c3f37e5479935e43490b6b2f3ef0713f9c535d
noam <clusterfoo@server.fake>
2013-11-07 14:34:02 -0500

    hjfhdsahfkjl

---
+++ ubuntu_packages.sh
@@ -0,0 +1,6 @@
+# Utilities
+apt-get install ack-grep vim git tig tree pstree tmux  clipit arandr baobab; 
+# compile tools
+apt-get install gcc-arm-none-eabi build-essential virtualbox;
+# math
+apt-get install sagemath-upstream-binary;
