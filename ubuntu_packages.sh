25b697e5fba52123f0808b5663d23fa5f1a19218
noam <clusterfoo@server.fake>
2013-11-07 15:12:08 -0500

    fshjfksa

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -1,6 +1,15 @@
 # Utilities
-apt-get install ack-grep vim git tig tree pstree tmux  clipit arandr baobab; 
+apt-get install ack-grep vim git tig tree pstree tmux calligra clipit arandr \
+dolphin baobab karbon kmix krita synaptic xchat; 
 # compile tools
-apt-get install gcc-arm-none-eabi build-essential virtualbox;
-# math
-apt-get install sagemath-upstream-binary;
+apt-get install  build-essential virtualbox;
+# math and python
+apt-get install python-dev python-numpy \
+python-setuptools python-scipy libatlas-dev libatlas3-base python-matplotlib;
+
+##
+# PPAS
+######
+# sagemath-upstream-binary 
+# gcc-arm-none-eabi
+# mongodb-10gen
