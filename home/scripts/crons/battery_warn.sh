086aeb662dc633cb8baba56de17d9b801ee6128f
noam <CLUSTERfoo@users.noreply.github.com>
2014-11-13 17:40:45 -0500

    fshjk

--- home/scripts/crons/battery_warn.sh
+++ home/scripts/crons/battery_warn.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Checks if battery is low and display message
+# add to cron: `crontab -e`
+#http://stackoverflow.com/questions/5398014/using-crontab-to-execute-script-every-minute-and-another-every-24-hours
+#
+# * * * * * ~/.scripts/crons/battery_warn.sh
+
 threshold=4
 level=$(acpi | cut -f2 -d, | cut -f1 -d%)
 
