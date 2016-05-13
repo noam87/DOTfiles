0c14fa39cd71b80e118cc64fe632d927ac918452
noam <noam87@users.noreply.github.com>
2016-05-13 13:53:39 -0400

    emacs stuff

--- home/emacs_scripts/load_packages.el
+++ home/emacs_scripts/load_packages.el
@@ -1,12 +1,21 @@
 (setq my-package-list
       '(neotree
+	auto-complete
+	elixir-mode
 	gotham-theme
-	suscolors-theme
-	quasi-monochrome-theme
-	monochrome-theme))
+	quasi-monochrome-theme))
 (mapc #'package-install my-package-list)
 
 ;; neotree
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 ;; neotree
+
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+;; auto-complete
+
+;; elixir-mode
+(require 'elixir-mode)
+;; elixir-mode
--- home/emacs_scripts/my_settings.el
+++ home/emacs_scripts/my_settings.el
@@ -1,1 +1,9 @@
+;; Show trailing whitespaces.
 (setq-default show-trailing-whitespace t)
+
+;; Add line number with space after.
+(global-linum-mode t)
+(setq linum-format "%d ")
+
+;; Easier file/buffer browsing.
+(ido-mode t)
